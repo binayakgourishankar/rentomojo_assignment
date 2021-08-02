f2041fa731896b4167743f5b8102292abf30c448
Binayak G Shankar <binayakgourishankar@gmail.com>
2021-08-02 07:43:25 +0530

    testing 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express=require("express"),
 
 mongoose.connect("mongodb://localhost/mydb");
 
-var CommentSchema=mongoose.Schema({
+const CommentSchema=mongoose.Schema({
    name:String,
    body:String,
    upvote: Number,
